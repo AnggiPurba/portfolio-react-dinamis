1678599b1339590a4c2ba4449af42df8c8b0588d
AnggiPurba <dwipurba77@gmail.com>
2025-08-07 10:28:31 +0700

    Perubahan CSS14

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,20 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import SubSectionHeading from './SubSectionHeading';
 import { urlFor } from '../sanityClient';
 
 const About = ({ data }) => {
-  const [open, setOpen] = useState(false);
   const contentRef = useRef(null);
   const personalInfo = data?.personalInfo || [];
   const midpoint = Math.ceil(personalInfo.length / 2);
   const firstHalf = personalInfo.slice(0, midpoint);
   const secondHalf = personalInfo.slice(midpoint);
 
-  // Untuk animasi height
+  // Pastikan konten expand otomatis sesuai isi
   useEffect(() => {
     if (contentRef.current) {
-      contentRef.current.style.maxHeight = open
-        ? `${contentRef.current.scrollHeight}px`
-        : '0px';
+      contentRef.current.style.maxHeight = `${contentRef.current.scrollHeight}px`;
     }
-  }, [open]);
+  }, []);
 
   return (
     <section id="about">
@@ -31,43 +28,41 @@ const About = ({ data }) => {
             <img src={urlFor(data.profileImage2).url()} alt="About Me" />
           )}
         </div>
+
         <div className="info-col">
           <SubSectionHeading title={data?.personal_info_title} />
           <p>{data?.personal_info_para}</p>
 
-          {/* === Accordion Header === */}
-          <button
-            className={`accordion-header ${open ? 'active' : ''}`}
-            onClick={() => setOpen(prev => !prev)}
-          >
-            <span>{open ? 'Hide Details' : 'Show Details'}</span>
-            <i className="arrow down"></i>
-          </button>
-
-          {/* === Accordion Content === */}
-          <div className="accordion-content" ref={contentRef}>
-            <div className="icon-list-col">
-              <div className="icon-list">
-                <ul>
-                  {firstHalf.map((item, idx) => (
-                    <li key={idx}>
-                      <i className="fa-solid fa-angle-right"></i>
-                      <strong>{item.label}:</strong>&nbsp;
-                      <span>{item.value}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="icon-list">
-                <ul>
-                  {secondHalf.map((item, idx) => (
-                    <li key={idx}>
-                      <i className="fa-solid fa-angle-right"></i>
-                      <strong>{item.label}:</strong>&nbsp;
-                      <span>{item.value}</span>
-                    </li>
-                  ))}
-                </ul>
+          {/* === Accordion Hover === */}
+          <div className="accordion-wrapper">
+            <div className="accordion-header">
+              <span>Personal Information</span>
+              <i className="arrow down"></i>
+            </div>
+            <div className="accordion-content" ref={contentRef}>
+              <div className="icon-list-col">
+                <div className="icon-list">
+                  <ul>
+                    {firstHalf.map((item, idx) => (
+                      <li key={idx}>
+                        <i className="fa-solid fa-angle-right"></i>
+                        <strong>{item.label}:</strong>&nbsp;
+                        <span>{item.value}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="icon-list">
+                  <ul>
+                    {secondHalf.map((item, idx) => (
+                      <li key={idx}>
+                        <i className="fa-solid fa-angle-right"></i>
+                        <strong>{item.label}:</strong>&nbsp;
+                        <span>{item.value}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
