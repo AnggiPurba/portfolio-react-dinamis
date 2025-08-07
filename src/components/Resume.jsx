8df5bec20785d97bce57770a9c632cc866268755
AnggiPurba <dwipurba77@gmail.com>
2025-08-07 12:47:52 +0700

    Perubahan CSS20

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import SubSectionHeading from './SubSectionHeading';
 
-// Fungsi sortTimeline tidak perlu diubah
 const sortTimeline = (items) => {
  const monthToNumber = (monthStr) => {
   const months = { jan: 1, feb: 2, mar: 3, apr: 4, may: 5, jun: 6, jul: 7, aug: 8, sep: 9, oct: 10, nov: 11, dec: 12 };
@@ -29,53 +28,52 @@ const Resume = ({ data }) => {
    <h1 className="sub-heading">{data.heading}</h1>
    <div className="divider"></div>
    <p className="sub-para">{data.sub_para}</p>
-   
-      {/* STRUKTUR BARU: Tiga bagian terpisah di dalam .resume-row */}
    <div className="resume-row">
-        
-        {/* Kolom 1: Professional Experience */}
-        <div className="resume-section">
-          <SubSectionHeading title={data.experience_title} />
-          <div className="timeline-wrapper">
-            {sortedExperience.map(exp => (
-              <div className="pro-exp" key={exp.id}>
-                <h3>{exp.title}</h3>
-                <span className="sp-box">{`${exp.startDate} - ${exp.endDate}`}</span>
-                <p>{exp.institution}</p>
-                <ul>
-                  {exp.details.map((detail, index) => (
-                    <li key={index}>{detail}</li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
-        </div>
 
-        {/* Kolom 2: Education */}
-        <div className="resume-section">
-          <SubSectionHeading title={data.education_title} />
-          <div className="timeline-wrapper">
-            {sortedEducation.map(edu => (
-              <div className="my-edu" key={edu.id}>
-                <h3>{edu.degree}</h3>
-                <span className="sp-box">{`${edu.startDate} - ${edu.endDate}`}</span>
-                <p>{edu.institution}</p>
-                <p>{edu.description}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-        
-        {/* Kolom 3: Summary */}
-        <div className="resume-section">
-          <SubSectionHeading title={data.summary_title} />
-          <div className="my-info">
-            <h3>{data.summary.title}</h3>
-            <p>{data.summary.content}</p>
-          </div>
-        </div>
+    {/* Kolom Kiri: Berisi Education dan Summary */}
+    <div className="resume-cols">
+       {/* Kolom Kanan: Berisi Experience */}
+    <div className="resume-cols">
+     <SubSectionHeading title={data.experience_title} />
+     <div className="timeline-wrapper">
+      {sortedExperience.map(exp => (
+       <div className="pro-exp" key={exp.id}>
+         <h3>{exp.title}</h3>
+         <span className="sp-box">{`${exp.startDate} - ${exp.endDate}`}</span>
+         <p>{exp.institution}</p>
+         <ul>
+         {exp.details.map((detail, index) => (
+          <li key={index}>{detail}</li>
+         ))}
+         </ul>
+       </div>
+      ))}
+     </div>
+    </div>
 
+     {/* ==== BAGIAN EDUCATION DIPINDAH KE ATAS ==== */}
+     <SubSectionHeading title={data.education_title} />
+     <div className="timeline-wrapper">
+      {sortedEducation.map(edu => (
+       <div className="my-edu" key={edu.id}>
+        <h3>{edu.degree}</h3>
+        <span className="sp-box">{`${edu.startDate} - ${edu.endDate}`}</span>
+        <p>{edu.institution}</p>
+        <p>{edu.description}</p>
+       </div>
+      ))}
+     </div>
+
+          {/* ==== BAGIAN SUMMARY SEKARANG DI BAWAH EDUCATION ==== */}
+     <SubSectionHeading title={data.summary_title} />
+     <div className="my-info">
+      <h3>{data.summary.title}</h3>
+      <p>{data.summary.content}</p>
+     </div>
+
+    </div>
+
+   
    </div>
   </section>
  );
